e3cc4f82fdd18ae16e991242f2b83e6625db3ad6
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-27 18:08:18 +0000

    Get rid of magic numbers

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,19 +1,19 @@
 class Oystercard
 	attr_reader :balance
-	DEFAULT_LIMIT = 90
+	MAXIMUM_BALANCE = 90
 
 	def initialize(balance = 0)
 		@balance = balance
 		@balance_limit = 90
 	end
 
-	def add_money(quantity)
-		raise 'The balance limit is 90 pounds' if balance_full?(quantity)
-		@balance += quantity
+	def add_money(amount)
+		raise 'The balance limit is 90 pounds' if balance_full?(amount)
+		@balance += amount
 	end
 
 	private
-	def balance_full?(quantity)
-		(@balance + quantity) > DEFAULT_LIMIT
+	def balance_full?(amount)
+		(@balance + amount) > MAXIMUM_BALANCE
 	end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,11 +6,13 @@ describe Oystercard do
 		expect(subject.balance).to eq 0
 	end
 
-	it 'can be loaded with money' do
+	it 'can top up' do
 		expect{ subject.add_money 5 }.to change{ subject.balance }.by 5
 	end
 
 	it 'has a balance limit of 90 Pounds' do
-		expect{ subject.add_money 91 }.to raise_error 'The balance limit is 90 pounds'
+		maximum_balance = Oystercard::MAXIMUM_BALANCE
+		subject.add_money maximum_balance
+		expect{ subject.add_money 1 }.to raise_error 'The balance limit is 90 pounds'
 	end
 end
