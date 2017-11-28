1844061db21039ba2e490fd83d76c12f3e25c564
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-28 22:14:10 +0000

    Refactoring

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,51 +1,56 @@
 require 'oystercard.rb'
 
 describe Oystercard do
+	context "initial status" do
+		it 'has no money at the beginning' do
+			expect(subject.balance).to eq 0
+		end
 
-	it 'has no money at the beginning' do
-		expect(subject.balance).to eq 0
+		it 'has a balance limit of 90 pounds' do
+			maximum_balance = Oystercard::MAXIMUM_BALANCE
+			subject.top_up maximum_balance
+			expect{ subject.top_up 1 }.to raise_error 'The balance limit is 90 pounds'
+		end
+
+		it "initially is not in journey" do
+			expect(subject.in_journey).to be false
+		end
 	end
 
-	it 'can top up' do
-		expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+	describe "#top_up & deduct" do
+		it 'can top up' do
+			expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+		end
+
+		it 'can deduct money' do
+			subject.top_up 5
+			subject.touch_in
+			expect {subject.touch_out}.to change{ subject.balance }.by (-Oystercard::MINIMUM_FARE)
+		end
 	end
 
-	it 'has a balance limit of 90 pounds' do
-		maximum_balance = Oystercard::MAXIMUM_BALANCE
-		subject.top_up maximum_balance
-		expect{ subject.top_up 1 }.to raise_error 'The balance limit is 90 pounds'
+	describe "#touch_in & out" do
+		it "touches in" do
+	    subject.top_up(5)
+			subject.touch_in
+			expect(subject.in_journey).to be true
+
+		end
+
+		it "touches out" do
+	    subject.top_up(5)
+			subject.touch_in
+			subject.touch_out
+			expect(subject.in_journey).to be false
+		end
 	end
 
-	it 'can deduct money' do
-		subject.top_up 5
-		subject.touch_in
-		expect {subject.touch_out}.to change{ subject.balance }.by (-Oystercard::MINIMUM_FARE)
-	end
-
-  it "initially is not in journey" do
-    expect(subject.in_journey).to be false
-  end
-
-	it "touches in" do
-    subject.top_up(5)
-		subject.touch_in
-		expect(subject.in_journey).to be true
-
-	end
-
-	it "touches out" do
-    subject.top_up(5)
-
-		subject.touch_in
-		subject.touch_out
-		expect(subject.in_journey).to be false
-
-	end
-
+context "insufficient funds" do
   it "raises an error if insufficient funds" do
     subject.balance <= Oystercard::MINIMUM_FARE
     expect { subject.touch_in }.to raise_error "Insufficient funds"
   end
+end
 
 
 end
